fc554d2633ea8d63ddb9a7026576f6e05a5f063d
kelsey edge <kelseyedge@kelseys-MacBook-Pro.local>
2018-02-14 13:19:52 -0500

    add crud pleasse

--- src/main/java/com/edge/demo/controller/CurrencyStub.java
+++ src/main/java/com/edge/demo/controller/CurrencyStub.java
@@ -1,47 +1,47 @@
-package com.boot.controller;
+package com.edge.demo.controller;
+
+import com.edge.demo.model.Currency;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.boot.model.Shipwreck;
-
-public class ShipwreckStub {
-	private static Map<Long, Shipwreck> wrecks = new HashMap<Long, Shipwreck>();
+public class CurrencyStub {
+	private static Map<Long, Currency> wrecks = new HashMap<Long, Currency>();
 	private static Long idIndex = 3L;
 
 	//populate initial wrecks
 	static {
-		Shipwreck a = new Shipwreck(1L, "U869", "A very deep German UBoat", "FAIR", 200, 44.12, 138.44, 1994);
+		Currency a = new Currency(1L, "U869", "A very deep German UBoat", "FAIR", 200.0, 44.12, 138.44, 1994);
 		wrecks.put(1L, a);
-		Shipwreck b = new Shipwreck(2L, "Thistlegorm", "British merchant boat in the Red Sea", "GOOD", 80, 44.12, 138.44, 1994);
+		Currency b = new Currency(2L, "Thistlegorm", "British merchant boat in the Red Sea", "GOOD", 80.0, 44.12, 138.44, 1994);
 		wrecks.put(2L, b);
-		Shipwreck c = new Shipwreck(3L, "S.S. Yongala", "A luxury passenger ship wrecked on the great barrier reef", "FAIR", 50, 44.12, 138.44, 1994);
+		Currency c = new Currency(3L, "S.S. Yongala", "A luxury passenger ship wrecked on the great barrier reef", "FAIR", 50.0, 44.12, 138.44, 1994);
 		wrecks.put(3L, c);
 	}
 
-	public static List<Shipwreck> list() {
-		return new ArrayList<Shipwreck>(wrecks.values());
+	public static List<Currency> list() {
+		return new ArrayList<Currency>(wrecks.values());
 	}
 
-	public static Shipwreck create(Shipwreck wreck) {
+	public static Currency create(Currency wreck) {
 		idIndex += idIndex;
 		wreck.setId(idIndex);
 		wrecks.put(idIndex, wreck);
 		return wreck;
 	}
 
-	public static Shipwreck get(Long id) {
+	public static Currency get(Long id) {
 		return wrecks.get(id);
 	}
 
-	public static Shipwreck update(Long id, Shipwreck wreck) {
+	public static Currency update(Long id, Currency wreck) {
 		wrecks.put(id, wreck);
 		return wreck;
 	}
 
-	public static Shipwreck delete(Long id) {
+	public static Currency delete(Long id) {
 		return wrecks.remove(id);
 	}
 }
