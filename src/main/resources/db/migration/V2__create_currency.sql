64446c703d1d251fe3132f169b19b82acf3e7515
Spann303 <samnang.pann@gmail.com>
2018-04-05 14:05:18 -0400

    Laid groundwork for User MVC

--- src/main/resources/db/migration/V2__create_currency.sql
+++ src/main/resources/db/migration/V2__create_currency.sql
@@ -1,9 +1,27 @@
-  CREATE TABLE CURRENCY(
-  id int AUTO_INCREMENT,
-  name VARCHAR(255),
-  symbol VARCHAR(255),
-  rank VARCHAR(255),
-  sod_price DOUBLE,
-  current_price DOUBLE,
-  date_time VARCHAR(255)
-  );
+CREATE TABLE CURRENCY(
+	id int AUTO_INCREMENT,
+	name VARCHAR(255),
+	symbol VARCHAR(255),
+	rank VARCHAR(255),
+	sod_price DOUBLE,
+	current_price DOUBLE,
+	date_time VARCHAR(255)
+);
+
+CREATE TABLE CREDENTIALS(
+	id INT AUTO_INCREMENT, 
+	username VARCHAR(255),
+	password VARCHAR(255),
+	PRIMARY KEY (id),
+	UNIQUE (username)
+);
+
+CREATE TABLE USER(
+	id INT,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	FOREIGN KEY (id) REFERENCES crendentials(id)
+);
+
+
+
