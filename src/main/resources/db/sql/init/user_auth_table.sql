adea358058534c7986d198d38d1938836efc41bb
Spann303 <samnang.pann@gmail.com>
2018-04-07 10:38:45 -0400

    Initialized sqlite3 DB of users.

---
+++ src/main/resources/db/sql/init/user_auth_table.sql
@@ -0,0 +1,26 @@
+PRAGMA foreign_keys = ON;
+
+drop table if exists user_roles;
+drop table if exists user;
+drop table if exists credentials;
+
+create table credentials (
+	id integer,
+	username text not null,
+	password text not null,
+	primary key (id),
+	unique (username)
+);
+
+create table user (
+	id integer,
+	first_name text not null,
+	last_name text not null,
+	foreign key (id) references credentials(id) on delete cascade 
+);
+
+create table user_roles (
+	id integer,
+	role text not null,
+	foreign key (id) references credentials(id) on delete cascade
+);
---
+++ src/main/resources/db/sql/init/user_data.sql
@@ -0,0 +1,23 @@
+insert into credentials (username, password) values ("admin","admin");
+insert into credentials (username, password) values ("qubuni@qub.ac.uk","password");
+insert into credentials (username, password) values ("kelsey","admin");
+insert into credentials (username, password) values ("austin","admin");
+insert into credentials (username, password) values ("sam","admin");
+insert into credentials (username, password) values ("michael","admin");
+insert into credentials (username, password) values ("user","admin");
+
+insert into user values (1, "admin", "admin");
+insert into user values (2, "qubuni", "email");
+insert into user values (3, "kelsey", "edge");
+insert into user values (4, "austin", "ray");
+insert into user values (5, "sam", "pann");
+insert into user values (6, "michael", "suggs");
+insert into user values (7, "user", "test");
+
+insert into user_roles values (1,"admin");
+insert into user_roles values (2,"user");
+insert into user_roles values (3,"admin");
+insert into user_roles values (4,"admin");
+insert into user_roles values (5,"admin");
+insert into user_roles values (6,"admin");
+insert into user_roles values (7,"user");
