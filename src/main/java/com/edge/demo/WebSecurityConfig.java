89af31c7db67d8732bf6e4c1acc7ece8d5cb1627
Spann303 <samnang.pann@gmail.com>
2018-04-06 22:51:06 -0400

    Configured inMemoryUserDetailsManager to read from a text file and format data according to the constraints

--- src/main/java/com/edge/demo/WebSecurityConfig.java
+++ src/main/java/com/edge/demo/WebSecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Properties;
 
 @Configuration
@@ -48,16 +51,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        final Properties users = new Properties();
-        String adminPassword = passwordEncoder().encode("admin");
-        users.put("admin", adminPassword + ",ROLE_ADMIN,enabled");
-        users.put("qubuni@qub.ac.uk", "password,ROLE_USER,enabled");
-        users.put("kelsey", adminPassword+",ROLE_ADMIN,enabled");
-        users.put("austin", adminPassword+",ROLE_ADMIN,enabled");
-        users.put("sam", adminPassword+",ROLE_ADMIN,enabled");
-        users.put("michael", adminPassword+",ROLE_ADMIN,enabled");
-        users.put("user", adminPassword+",ROLE_USER,enabled");
-        //add whatever other user you need
+        
+    	final Properties users = new Properties();
+    	/**
+    	 * Pulling data from a text file to add users to the DB.
+    	 */
+    	
+    	File file = new File("/src/main/resources/db/users.txt");
+    	
+    	try {
+    		BufferedReader bf = new BufferedReader(new FileReader(file));
+    		String line = bf.readLine();
+    		
+    		while (line != null) {
+    			String [] line_split = line.trim().split("\t");
+    			users.put(line_split[0],passwordEncoder().encode(line_split[1])+","+line_split[2]);
+    			line = bf.readLine();
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	
+//    	final Properties users = new Properties();
+//        String adminPassword = passwordEncoder().encode("admin");
+//        users.put("admin", adminPassword + ",ROLE_ADMIN,enabled");
+//        users.put("qubuni@qub.ac.uk", "password,ROLE_USER,enabled");
+//        users.put("kelsey", adminPassword+",ROLE_ADMIN,enabled");
+//        users.put("austin", adminPassword+",ROLE_ADMIN,enabled");
+//        users.put("sam", adminPassword+",ROLE_ADMIN,enabled");
+//        users.put("michael", adminPassword+",ROLE_ADMIN,enabled");
+//        users.put("user", adminPassword+",ROLE_USER,enabled");
+//        //add whatever other user you need
 
         return new InMemoryUserDetailsManager(users);
     }
@@ -66,4 +90,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    		
 }
