755e2e851e5d5480a474eeaf37f0927eaa3095a3
kelsey edge <kmedge22@yahoo.com>
2018-04-16 22:09:14 -0400

    Bug Fix: css problem preventing response from user clicking view or delete on the Portfolio tab. Added a margin-top property to fix

--- src/main/resources/db/migration/V2__create_currency.sql
+++
@@ -1,9 +0,0 @@
-  CREATE TABLE CURRENCY(
-  id int AUTO_INCREMENT,
-  name VARCHAR(255),
-  symbol VARCHAR(255),
-  rank VARCHAR(255),
-  sod_price DOUBLE,
-  current_price DOUBLE,
-  date_time VARCHAR(255)
-  );
--- src/test/java/com/edge/demo/DemoApplicationTests.java
+++ src/test/java/com/edge/demo/DemoApplicationTests.java
@@ -1,6 +1,9 @@
 package com.edge.demo;
 
 //
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
 //public class DemoApplicationTests {
